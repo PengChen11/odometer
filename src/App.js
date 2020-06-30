4d9923d747d6a4771bc9d26b21da134108d9ac01
Peng Chen <pchen0816@outlook.com>
2020-06-30 14:18:52 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class Header extends React.Component{
   render(){
-    return <h1>Hello, {this.props.name}!</h1>;
+    return <h1>Welcome to {this.props.name}'s Odometer app!</h1>;
+  };
+};
+
+class Footer extends React.Component{
+  render(){
+    return <p>{this.props.trademark} &trade;</p>
+  };
+};
+
+class Odometer extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      reading: 0
+    };
+  }
+  
+  updateOdometer(change){
+    this.setState({
+      reading: this.state.reading + change
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <div className='digits'>
+          <p className='numbers'>{Math.floor(this.state.reading%10000 /1000)} </p>
+          <button onClick = {()=>this.updateOdometer(1000)}>Increase</button>
+        </div>
+        <div className='digits'> 
+          <p className='numbers'>{Math.floor(this.state.reading%1000 /100)} </p>
+          <button onClick = {()=>this.updateOdometer(100)}>Increase</button>
+        </div>
+        <div className='digits'>
+          <p className='numbers'>{Math.floor(this.state.reading %100 /10)} </p>
+          <button onClick = {()=>this.updateOdometer(10)}>Increase</button>
+        </div>
+        <div className='digits'>
+          <p className='numbers'>{this.state.reading % 10} </p>
+          <button onClick = {()=>this.updateOdometer(1)}>Increase</button>
+        </div>
+      </div>
+    );
   }
 }
 
-
-
 function App() {
-
-
   return (
     <div>
       <header>
-        < Header name='Peter' />
+        < Header name='Peng' />
       </header>
       <main>
-      this is main
+        < Odometer />
       </main>
       <footer>
-      this is footer
+        < Footer trademark='pengchen.work' />
       </footer>
     </div>  
   );
 }
-
-
-
-
-
-
-
-
 
 export default App;
 
