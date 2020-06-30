591b0768e5bb2b5a6994ce159d696ceb6a4e8022
Peng Chen <pchen0816@outlook.com>
2020-06-30 11:17:18 -0700

    init

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Header extends React.Component{
+  render(){
+    return <h1>Hello, {this.props.name}!</h1>;
+  }
+}
+
+
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        < Header name='Peter' />
       </header>
-    </div>
+      <main>
+      this is main
+      </main>
+      <footer>
+      this is footer
+      </footer>
+    </div>  
   );
 }
 
+
+
+
+
+
+
+
+
 export default App;
+
